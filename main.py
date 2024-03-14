72312256d047fd16bf04ea42f776ad6c7cab6164
Mayank Rai <mayankraivns@gmail.com>
2024-03-14 23:44:21 +0530

    reg-login

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Body, Form, Depends, HTTPException, status, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pymongo import MongoClient
@@ -24,14 +24,12 @@ db = conn.votingsys
 
 @app.get("/", tags = ["greet"])
 def greet(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("login.html", {"request": request})
     
     
-@app.get("/data", response_class = JSONResponse, tags=["data"])
-def get_users():
-    data = conn.votingsys.users.find({})
-    user = data["fullname"]
-    return user
+@app.get("/register", response_class=HTMLResponse, tags=["data"])
+def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
 
 @app.get("/data/{email}", tags=["data"])
 def get_one_user(email: EmailStr):
@@ -39,28 +37,27 @@ def get_one_user(email: EmailStr):
     name =  data["fullname"]
     return {"fullname": name}
 
-@app.post("/user/register", tags=["user"])
+@app.post("/user/register", response_class=HTMLResponse, tags=["user"])
 async def user_register(user: UserRegSchema = Depends(UserRegSchema.form)):
     data = user.model_dump()
     conn.votingsys.users.insert_one(data)
-    return {"message": "User registered successfully!"}
+    return RedirectResponse(url="/user/login")
 
 def check_user(data: UserLoginSchema):
     users = conn.votingsys.users.find({})
     for user in users:
-        if user.email == data.email:
-            if user.password == data.password:
+        if user["email"] == data.email:
+            if user["password"] == data.password:
                 return True
     return False
 
-@app.post("/user/login", tags=["user"])
-def user_login(user: UserLoginSchema = Depends(UserLoginSchema.form)):
+@app.post("/user/login", response_class=HTMLResponse, tags=["user"])
+def user_login(request: Request, user: UserLoginSchema = Depends(UserLoginSchema.form)):
     if check_user(user):
-        return {"message": "Logged in!"}
+        return templates.TemplateResponse("dashboard.html", {"request": request})
     else:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
     
 if __name__ == "__main__":
     uvicorn.run(app, port=8004, host="127.0.0.1")
-    print("Server started at http://LULW:8004/")
