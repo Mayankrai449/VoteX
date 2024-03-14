dbf34a855c8d392a9853ee8b80417f5c307b8c39
Mayank Rai <mayankraivns@gmail.com>
2024-03-14 13:42:47 +0530

    registration auth

--- main.py
+++ main.py
@@ -1,65 +1,39 @@
-from fastapi import FastAPI, Body, Depends, HTTPException, status
+from fastapi import FastAPI, Body, Form, Depends, HTTPException, status, Request
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
+from typing import Annotated
+
 import uvicorn
 from models.model import DataSchema, UserRegSchema, UserLoginSchema
 from auth.jwt_handler import signJWT
 from auth.jwt_bearer import jwtBearer
-import config
+import control
 
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-data = [
-    {
-    "id": 1,
-    "name": "mayank",
-    "age": "21",
-    "email": "mayankrai@gmail.com"
-    },
-    {
-    "id": 2,
-    "name": "anuj",
-    "age": "19",
-    "email": "bing@gmail.com"
-    }
-]
-
-users = []
+conn = MongoClient("mongodb+srv://Mayankrai449:RWHLI4g2RqoHljpQ@cluster0.7hu8wbd.mongodb.net/votingsys")
+db = conn.votingsys
 
 
 @app.get("/", tags = ["greet"])
-def greet():
-    return {
-        "Hello": "Mayank"
-    }
+def greet(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
     
-@app.get("/data", tags = ["data"])
-def fetch_all_data():
-    return {"data": data}
-
-@app.get("/data/{id}", tags = ["data"])
-def fetch_one_data(id: int):
-    if id > len(data):
-        return {"Invalid id"}
-    for d in data:
-        if d["id"] == id:
-            return {"Single_data": d}
-    return {"data not found"}
-
-@app.post("/post", dependencies=[Depends(jwtBearer())], tags=["posts"])
-def post_data(data_set: DataSchema):
-    data_set.id = len(data) + 1
-    data.append(data_set.model_dump())
-    return {"data posted!"}
 
 @app.post("/user/register", tags=["user"])
-def user_register(user: UserRegSchema = Body(default=None)):
-    user.id = len(users) + 1
-    user.age = config.calculate_age(user.dob)
-    users.append(user)
-    return signJWT(user.email)
+async def user_register(user: UserRegSchema) -> UserRegSchema:
+    data = user.model_dump()
+    conn.votingsys.users.insert_one(data)
+    return {"message": "User registered successfully!"}
 
 def check_user(data: UserLoginSchema):
+    users = conn.votingsys.users.find({})
     for user in users:
         if user.email == data.email:
             if user.password == data.password:
@@ -67,7 +41,7 @@ def check_user(data: UserLoginSchema):
     return False
 
 @app.post("/user/login", tags=["user"])
-def user_login(user: UserLoginSchema = Body(default=None)):
+def user_login(user: UserLoginSchema):
     if check_user(user):
         return {"message": "Logged in!"}
     else:
--- models/model.py
+++ models/model.py
@@ -7,47 +7,16 @@ class DataSchema(BaseModel):
     age: int = Field(default=None),
     email: EmailStr = Field(default=None),
     
-    class config:
-        external_ref = {
-            "demo": {
-                "id": 1,
-                "name": "mayank",
-                "age": 21,
-                "email": "raimayank@gmail.com"
-            }
-        }
-
 
 class UserRegSchema(BaseModel):
-    id: int = Field(default=None),
-    fullname: str = Field(default=None),
-    dob: date = Field(default=None),
-    age: int = Field(default=None),
-    email: EmailStr = Field(default=None),
+    fullname: str
+    email: EmailStr
+    dob: str = Field(default=None),
     city: str = Field(default=None),
-    password: str = Field(default=None),
+    password: str
     
-    class config:
-        external_ref = {
-            "demo": {
-                "id": 1,
-                "fullname": "mayank",
-                "dob": date(2000, 1, 1),
-                "age": 24,
-                "email": "sample@email.com",
-                "city": "delhi",
-                "password": "pass123"
-            }
-        }
  
 class UserLoginSchema(BaseModel):
     email: EmailStr = Field(default=None),
     password: str = Field(default=None),
     
-    class config:
-        external_ref = {
-            "demo": {
-                "username": "mayank",
-                "password": "pass123"
-            }
-        }
