0d5ad0981aef1e3c822e46875ccaa892c029b9e1
Mayank Rai <mayankraivns@gmail.com>
2024-04-25 20:57:59 +0530

    flash 2

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI, Body, Form, Depends, HTTPException, status, Request
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse, Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Cookie
 
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
-from typing import Union
+from typing import Union, Optional
 from typing_extensions import Annotated
 from datetime import datetime, timedelta, timezone
 import pytz
@@ -42,28 +43,21 @@ local_timezone = pytz.timezone('Asia/Kolkata')
 async def greet(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
-@app.get("/flash", response_class=HTMLResponse)
-async def home(request: Request):
-    flash_message = "Yolo, this is my flash!"
-    flash_type = "success"  # Assuming you want to pass the flash message type as well
-    return templates.TemplateResponse("flash.html", {"request": request, "flash_message": flash_message, "flash_type": flash_type})
-
 @app.get("/layout", response_class=HTMLResponse, tags=["greet"])
 async def layout(request: Request):
     return templates.TemplateResponse("layout.html", {"request": request})
 
-
-@app.get("/data/{username}", tags=["data"])
-def get_one_user(username: str):
-    data = db.users.find_one({"username": username})
-    name =  data["fullname"]
-    return {"fullname": name}
-
 @app.get("/dashboard", tags=["data"])
-async def dashboard(request: Request, current_user: dict = Depends(get_current_active_user)):
-    flash_message = "Yolo, this is my flash!"
-    flash_type = "success" 
-    return templates.TemplateResponse("dashboard.html", {"request": request, "token": current_user, "flash_message": flash_message, "flash_type": flash_type})
+async def dashboard(request: Request, current_user: dict = Depends(get_current_active_user),
+                    flash_message: Optional[str] = Cookie(None),
+                    flash_type: Optional[str] = Cookie(None)):
+    response = templates.TemplateResponse("dashboard.html", {"request": request, "token": current_user,
+                                                             "flash_message": flash_message,
+                                                             "flash_type": flash_type})
+    response.set_cookie(key="flash_message", value="", httponly=True)
+    response.set_cookie(key="flash_type", value="", httponly=True)
+    
+    return response
 
 @app.get("/history", tags=["data"])
 async def history(request: Request, current_user: dict = Depends(get_current_active_user)):
@@ -82,12 +76,11 @@ async def history(request: Request, current_user: dict = Depends(get_current_act
     return templates.TemplateResponse("results.html", {"request": request, "results": results, "token": current_user})
 
 
-@app.post("/createpoll", tags = ["poll"])
+@app.post("/createpoll", tags=["poll"])
 async def create_poll(data: PollForm = Depends(PollForm.form), current_user: dict = Depends(get_current_active_user)):
     try:
         end_datetime_str = f"{data.end_date} {data.end_time}"
         end_datetime = datetime.strptime(end_datetime_str, "%m/%d/%Y %H:%M")
-        
         end_datetime_local = local_timezone.localize(end_datetime)
         
         poll = data.model_dump()
@@ -100,9 +93,16 @@ async def create_poll(data: PollForm = Depends(PollForm.form), current_user: dic
         
         db.history.insert_one(poll)
         
-        return {"message": "Poll created successfully"}
+        response = RedirectResponse(url="/polls", status_code=status.HTTP_303_SEE_OTHER)
+        response.set_cookie(key="flash_message", value="Poll created successfully!", httponly=True)
+        response.set_cookie(key="flash_type", value="success", httponly=True)
+        
+        return response
     except Exception as e:
-        return {"error": str(e)}
+        response = RedirectResponse(url="/pollform", status_code=status.HTTP_303_SEE_OTHER)
+        response.set_cookie(key="flash_message", value=str(e), httponly=True)
+        response.set_cookie(key="flash_type", value="failure", httponly=True)
+        return response
 
 
 @app.get("/pollform", tags=["poll"])
@@ -111,10 +111,18 @@ async def poll_form(request: Request, current_user: dict = Depends(get_current_a
 
 
 @app.get("/polls", tags=["poll"])
-async def get_polls(request: Request, current_user: dict = Depends(get_current_active_user)):
+async def get_polls(request: Request, current_user: dict = Depends(get_current_active_user),
+                    flash_message: Optional[str] = Cookie(None),
+                    flash_type: Optional[str] = Cookie(None)):
     polls = db.polls.find({"creator": current_user["username"]})
-    return templates.TemplateResponse("allPolls.html", {"request": request, "polls": polls, "token": current_user})
-
+    response = templates.TemplateResponse("allPolls.html", {"request": request,
+                                                        "polls": polls, "token": current_user,
+                                                        "flash_message": flash_message,
+                                                        "flash_type": flash_type})
+    response.set_cookie(key="flash_message", value="", httponly=True)
+    response.set_cookie(key="flash_type", value="", httponly=True)
+    
+    return response
 
 @app.get("/poll/{poll_id}", tags=["poll"])
 async def get_poll(request: Request, poll_id: str, current_user: dict = Depends(get_current_active_user)):
@@ -194,13 +202,23 @@ async def update_poll(poll_id: str, data: PollForm = Depends(PollForm.form), cur
 
 
 @app.delete("/deletepoll/{poll_id}", tags=["poll"])
-async def delete_poll(poll_id: str, current_user: dict = Depends(get_current_active_user)):
+async def delete_poll(response: Response, poll_id: str, current_user: dict = Depends(get_current_active_user)):
     poll = db.polls.find_one({"poll_id": poll_id})
+    response = RedirectResponse(url="/polls", status_code=status.HTTP_303_SEE_OTHER)
     if not poll:
-        raise HTTPException(status_code=404, detail="Poll not found")
+        flash_message = "Poll Not Found!"
+        flash_type = "failure"
+
+        return response
     
     db.polls.delete_one({"poll_id": poll_id})
-    return RedirectResponse(url="/polls", status_code=status.HTTP_303_SEE_OTHER)
+    flash_message = "Poll Deleted Successfully!"
+    flash_type = "success"
+    
+    response.set_cookie(key="flash_message", value=flash_message, httponly=True)
+    response.set_cookie(key="flash_type", value=flash_type, httponly=True)
+    
+    return response
     
     
 
@@ -222,19 +240,24 @@ async def user_register(user: UserRegSchema = Depends(UserRegSchema.form)):
 @app.post("/login", tags=["user"])
 async def token_auth(response: Response, user: Annotated[OAuth2PasswordRequestForm, Depends()]):
     if not auth.check_user(user):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"})
-
+        response = RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
+        flash_message = "Incorrect username or password"
+        flash_type = "failure"
+    else:
+        response = RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
+        flash_message = "Login Successful!"
+        flash_type = "success"
+        
     access_token_expires = timedelta(minutes=auth.TOKEN_TIME)
     access_token = auth.create_access_token(
         data={"sub": user.username},
         expires_delta=access_token_expires
     )
-
-    response = RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
+    
+    response.set_cookie(key="flash_message", value=flash_message, httponly=True)
+    response.set_cookie(key="flash_type", value=flash_type, httponly=True)
     response.set_cookie(key="access_token", value=access_token, httponly=True)
+    
     return response
     
 @app.get("/logout", tags=["user"])
